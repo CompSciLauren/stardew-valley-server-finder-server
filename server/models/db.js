313b4388dc55ecde0476326c99c6d14f0524e36e
Lauren Stephenson <compscilauren@gmail.com>
2020-05-07 19:36:12 -0500

    Use JawsDB url

--- server/models/db.js
+++ server/models/db.js
@@ -1,18 +1,23 @@
-const mysql = require("mysql");
-const dbConfig = require("../config/db.config.js");
+const mysql = require('mysql');
+const dbConfig = require('../config/db.config.js');
 
+let connection;
 // Create a connection to the database
-const connection = mysql.createConnection({
-  host: dbConfig.HOST,
-  user: dbConfig.USER,
-  password: dbConfig.PASSWORD,
-  database: dbConfig.DB
-});
+if (process.env.JAWSDB_URL) {
+  mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: dbConfig.HOST,
+    user: dbConfig.USER,
+    password: dbConfig.PASSWORD,
+    database: dbConfig.DB,
+  });
+}
 
 // open the MySQL connection
-connection.connect(error => {
+connection.connect((error) => {
   if (error) throw error;
-  console.log("Successfully connected to the database.");
+  console.log('Successfully connected to the database.');
 });
 
 module.exports = connection;
