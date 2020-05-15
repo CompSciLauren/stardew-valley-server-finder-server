b19b1b6bcd8f689af78e78d678bbf824b0b3249d
Grant L Stewart <40806357+grantlaws@users.noreply.github.com>
2020-05-15 14:29:11 -0500

    Updated Query
    Please test before PR

--- server/models/player.model.js
+++ server/models/player.model.js
@@ -45,10 +45,12 @@ Player.updateUsername = (username, playerId, result) => {
 
 Player.findByID = (PlayerId, result) => {
   sql.query(
-    `SELECT id, username, timezone, num_players, platform, mods, notes, player_status.status_id FROM player
+    `SELECT id, username, timezone, num_players, platform.display as 'platform', mods, notes, status FROM (SELECT id as id2, username, timezone, num_players, platform, mods, notes, player_status.status_id as status FROM player
     JOIN player_status
     ON player.id = player_status.player_id
-    WHERE id = ?`,
+    WHERE id = ?) as P
+   JOIN platform
+   ON P.platform = platform.id`,
     PlayerId,
     (err, res) => {
       if (err) {
